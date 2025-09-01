da96e25bedab6961f078b69f18460fcd24855deb
Rizwaan POPAT <rizwaan.popat@osmium.solutions>
2025-09-01 12:31:30 +0200

    Initial portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://rizou95.github.io',
+  base: '/portfolio/', // repo name if not root
+  adapter: github(),
+});
+
