eba3c9cefe694ae999abe940b26735732ef6c9bf
Rizwaan POPAT <rizwaan.popat@osmium.solutions>
2025-09-01 13:09:03 +0200

    fix: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://rizou95.github.io',
-  base: '/portfolio/', // repo name if not root
-  adapter: github(),
+  base: '/portfolio', // repo name if not root
 });
 
