b6069a49c8ef02d60b0de7fbc8063c4aa5d7e614
Rizwaan POPAT <rizwaan.popat@osmium.solutions>
2025-09-03 11:05:40 +0200

    fix: base name needed for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rizou95.github.io'
+  site: 'https://rizou95.github.io',
+  base: '/portfolio', // repo name if not root
 });
 
